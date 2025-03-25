1c7f3f646377cfc8867edc717bd4e98e8fa2b046
yeonseoGit <140489064+yeonseoGit@users.noreply.github.com>
2025-03-25 12:19:32 +0900

    [Bronze III] Title: 웰컴 키트, Time: 36 ms, Memory: 34536 KB -BaekjoonHub

---
+++ 백준/Bronze/30802. 웰컴 키트/웰컴 키트.py
@@ -0,0 +1,19 @@
+import sys
+import math
+
+input = sys.stdin.readline
+
+n = int(input())
+
+sizes = map(int, input().split())
+
+t, p = map(int, input().split())
+
+shirts_set_count = 0
+
+for size in sizes:
+    if size > 0:
+        shirts_set_count += math.ceil(size / t)
+
+print(shirts_set_count)
+print(n // p, n % p)
