6f8860aa6d3008eb1df41de433c5a8925889c86f
yeonseoGit <140489064+yeonseoGit@users.noreply.github.com>
2025-03-28 13:57:25 +0900

    [Bronze II] Title: Hashing, Time: 36 ms, Memory: 32544 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.py
+++ 백준/Bronze/15829. Hashing/Hashing.py
@@ -1,10 +1,12 @@
 n = int(input())
 L = list(input())
+M = 1234567891
 
 sum_n = 0
 
-for i in range(0, n):
-    aa = ord(L[i])-96
+for i in range(n):
+    aa = ord(L[i]) - 96
     sum_n += aa * (31**i)
+    sum_n %= M 
 
 print(sum_n)
