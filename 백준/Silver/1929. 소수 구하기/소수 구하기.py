df17826ef61ced5fbce4d22fdadb9a7ca5575c35
yeonseoGit <140489064+yeonseoGit@users.noreply.github.com>
2025-04-01 18:18:18 +0900

    [Silver III] Title: 소수 구하기, Time: 292 ms, Memory: 40084 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.py
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -1,15 +1,19 @@
 import sys
-import math
+
 Input = sys.stdin.readline
 M, N = map(int, Input().split())
 
-for i in range(M, N+1):
-    if i < 2:
-        continue
-    is_prime = True
-    for j in range(2, int(math.isqrt(i)) + 1):
-        if i % j == 0:
-            is_prime = False
-            break
-    if is_prime:
+# 소수 여부 저장 배열 (True로 초기화)
+is_prime = [True] * (N + 1)
+is_prime[0] = is_prime[1] = False  # 0, 1은 소수가 아님
+
+# 소수 판별
+for i in range(2, int(N**0.5) + 1):
+    if is_prime[i]:
+        for j in range(i * i, N + 1, i):
+            is_prime[j] = False
+
+# 결과 출력
+for i in range(M, N + 1):
+    if is_prime[i]:
         print(i)
