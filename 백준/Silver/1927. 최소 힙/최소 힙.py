4f152f78c52ed1cd8b6fb0edad7225f9f8f4b145
yeonseoGit <140489064+yeonseoGit@users.noreply.github.com>
2025-04-10 11:09:04 +0900

    [Silver II] Title: 최소 힙, Time: 112 ms, Memory: 39124 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.py
@@ -0,0 +1,18 @@
+import sys
+import heapq
+
+input = sys.stdin.readline
+
+n = int(input())
+result = []
+x = [int(input()) for _ in range(n)]
+
+for i in range(0, len(x)):
+    if x[i] == 0 :
+        
+        if result:
+            print(heapq.heappop(result))
+        else :
+            print(0)
+    else :
+        heapq.heappush(result, x[i])
