d59315a5ca3b2b0c8bbc3af852589c6f13fa5525
yeonseoGit <140489064+yeonseoGit@users.noreply.github.com>
2025-04-15 14:10:06 +0900

    [Silver I] Title: 미로 탐색, Time: 68 ms, Memory: 34944 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,34 +1,28 @@
+import sys
 from collections import deque
 
-n, m = map(int, input().split())
+input = sys.stdin.readline
+queue = deque()
 
-graph = []
+N, M = map(int, input().split())
+miro = [list(map(int, input().strip())) for _ in range(N)]
 
-for i in range(n):
-    graph.append(list(map(int, input())))
-
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
+dx = [0, 0, 1, -1]
+dy = [1, -1, 0, 0]
 
 def bfs(x, y):
-    queue = deque()
     queue.append((x, y))
-    
-    while queue:
+    while queue :
         x, y = queue.popleft()
         
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
-            
-            if nx < 0 or nx >= n or ny < 0 or ny >= m:
-                continue
-            if graph[nx][ny] == 0:
-                continue
-            if graph[nx][ny] == 1:
-                graph[nx][ny] = graph[x][y] + 1
-                queue.append((nx, ny))
-    
-    return graph[n-1][m-1]
+
+            if 0 <= nx < N and 0 <= ny < M:
+                if miro[nx][ny] == 1 :
+                    miro[nx][ny] = miro[x][y] + 1
+                    queue.append((nx, ny))
+    return miro[N-1][M-1]
 
 print(bfs(0, 0))
