424af94d9ba9552f4641598b4c7414e4dc0dc5f4
yeonseoGit <140489064+yeonseoGit@users.noreply.github.com>
2025-05-15 10:36:18 +0900

    [Silver III] Title: 바이러스, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,20 +1,28 @@
 import sys
+input = sys.stdin.readline
 
-Input = sys.stdin.readline
+# 입력받기
+compute_n = int(input())
+ssang = int(input())
+virus = [[] for _ in range(compute_n)]
 
-N = int(Input())
-cup = int(Input())
-virus = [[] for _ in range(N)]
-for _ in range(cup):
-    a, b =  map(int, Input().split())
+for _ in range(ssang) :
+    a, b = map(int, input().split())
     virus[a-1].append(b-1)
     virus[b-1].append(a-1)
 
-visit = [False for _ in range(N)]
+# visit 구현
+visit = [False]*compute_n
+
+cnt_d = 0
+
 def dfs(x):
+    global cnt_d
     visit[x] = True
-    for next in virus[x]:
-        if not visit[next]:
-            dfs(next)
-dfs(0)  
-print(visit.count(True) - 1)  
+    for i in virus[x]:
+        if not visit[i]:
+            cnt_d += 1
+            dfs(i)
+
+dfs(0)
+print(cnt_d)
