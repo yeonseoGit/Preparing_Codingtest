f7a2537f08f8c1eac208ffc087369f7e8338588e
yeonseoGit <140489064+yeonseoGit@users.noreply.github.com>
2025-05-20 11:20:04 +0900

    [Silver I] Title: 안전 영역, Time: 596 ms, Memory: 229696 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.py
+++ 백준/Silver/2468. 안전 영역/안전 영역.py
@@ -1,42 +1,35 @@
-# 장마철에 내리는 비의 양에 따라 일정한 높이 이하의 모든 지점은 물에 잠긴다고 가정
+# dfs로 풀었을 때!
 import sys
-from collections import deque
-#bfs
+sys.setrecursionlimit(10000)  # ← 충분히 넉넉하게
 input = sys.stdin.readline
+
 n = int(input())
-local = [list(map(int, input().split())) for _ in range(n)]
+space = [list(map(int, input().split())) for _ in range(n)]
 
-# 좌우상하
-dx = [0, 0, -1, 1]
-dy = [-1, 1, 0, 0]
+dx = [1, -1, 0, 0]
+dy = [0, 0, 1, -1]
 
-# local 내 최대 높이 찾기
-max_height = max(map(max, local))
-answer = 0
+def dfs(x, y, h, visit):
+    visit[x][y] = True
+    for i in range(4):
+        nx = x+dx[i]
+        ny = y+dy[i]
+        if 0 <= nx < n and 0 <= ny < n :
+            if visit[nx][ny] == False and space[nx][ny] > h:
+                dfs(nx, ny, h, visit)
 
-def bfs(x, y, h, visited):
-    q = deque()
-    q.append((x, y))
-    visited[x][y] = True # 2차원 배열이니깐!
+m_h = max(map(max, space))
+ans = 0
 
-    while q:
-        cx, cy = q.popleft()
-        for d in range(4):
-            nx, ny = cx + dx[d], cy + dy[d]
-            if 0 <= nx < n and 0 <= ny < n:
-                if not visited[nx][ny] and local[nx][ny] > h:
-                    visited[nx][ny] = True
-                    q.append((nx, ny))
+for h in range(0, m_h):
+    visit = [[False]*n for _ in range(n)]
+    cnt = 0 
 
-for h in range(0, max_height + 1):  # 비가 하나도 안올 때부터 최대 높이까지
-    visited = [[False]*n for _ in range(n)]
-    cnt = 0  # 안전 영역 수
-
-    for i in range(n):
-        for j in range(n):
-            if not visited[i][j] and local[i][j] > h:
-                bfs(i, j, h, visited)
+    for i in range(0, n):
+        for j in range(0, n):
+            if visit[i][j] == False and space[i][j] > h:
+                dfs(i, j, h, visit)
                 cnt += 1
-    answer = max(answer, cnt)
+    ans = max(ans, cnt)
 
-print(answer)
+print(ans)
