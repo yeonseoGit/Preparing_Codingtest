9902c7ea38e61ed3f30a8d813159db02820b20cc
yeonseoGit <140489064+yeonseoGit@users.noreply.github.com>
2025-06-19 13:37:47 +0900

    [Bronze III] Title: 사격 내기, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/27960. 사격 내기/사격 내기.py
+++ 백준/Bronze/27960. 사격 내기/사격 내기.py
@@ -1,10 +1,43 @@
 import sys
+from itertools import combinations
 
 input = sys.stdin.readline
+
+lst = [1, 2, 4, 8, 16, 32, 64, 128, 256, 512]
 
 a, b = map(int, input().split())
 
 if a == b:
     print(0)
-else:
-    print(a ^ b)
+    exit()
+
+a_list = []
+for r in range(1, len(lst) + 1):
+    for comb in combinations(lst, r):
+        if sum(comb) == a:
+            a_list = list(comb)
+            break
+    if a_list:
+        break
+
+b_list = []
+for r in range(1, len(lst) + 1):
+    for comb in combinations(lst, r):
+        if sum(comb) == b:
+            b_list = list(comb)
+            break
+    if b_list:
+        break
+
+# 이제 a_list 와 b_list 비교
+result_c = []
+
+for bullet in a_list:
+    if bullet not in b_list:
+        result_c.append(bullet)
+
+for bullet in b_list:
+    if bullet not in a_list:
+        result_c.append(bullet)
+
+print(sum(result_c))
